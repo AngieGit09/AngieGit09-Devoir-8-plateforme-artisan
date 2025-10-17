763ae69ce947722680f0b81c6658f3e978d62e50
Angelique Liard <liardangelqiue@gmail.com>
2025-10-17 17:00:05 +0200

    fichier redirects pour netlify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./style/index.scss";
 import App from "./App";
 
+
 const rootEl = document.getElementById("root");
 if (!rootEl)
   throw new Error("Élément #root introuvable dans public/index.html");
